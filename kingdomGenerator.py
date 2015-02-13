2a9d929255ad92d8c7d3a91026222c332e773546
Refbot <chris.bartolomei@gmail.com>
2015-02-13 17:24:25 -0600

    Start to Intrigue Set, unit tests, a few utility functions added to client

--- kingdomGenerator.py
+++ kingdomGenerator.py
@@ -1,5 +1,6 @@
 import card as crd
 import base_set as base
+import intrigue_set as intrigue
 import inspect
 import random
 
@@ -7,16 +8,20 @@ class kingdomGenerator():
 	def __init__(self, game):
 		self.game = game
 		self.avail_cards = []
-		for name, obj in inspect.getmembers(base):
-			if inspect.isclass(obj):
-				self.avail_cards.append(obj(game, None))
+		# self.load_set(base)
+		self.load_set(intrigue)
 
 	def random_kingdom(self):
 		kingdom = []
-		for i in range(0, 10):
+		for i in range(0, 8):
 			selected_index = random.randint(0,len(self.avail_cards)-1)
 			kingdom.append(self.avail_cards.pop(selected_index))
 		return kingdom
+
+	def load_set(self, card_set):
+		for name, obj in inspect.getmembers(card_set):
+			if inspect.isclass(obj):
+				self.avail_cards.append(obj(self.game, None))
 
 
 def all_cards(game):
