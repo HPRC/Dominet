60e4752536618d395cc2cbf53d4a0a70d4b82ed7
Ryan Collins <rcollins@apperian.com>
2015-07-13 16:42:31 -0400

    Fixed hinterlands test

--- tests/hinterlands_tests.py
+++ tests/hinterlands_tests.py
@@ -33,26 +33,18 @@ class TestHinterland(unittest.TestCase):
 		crossroads = hl.Crossroads(self.game, self.player1)
 		tu.add_many_to_hand(self.player1, crossroads, 2)
 		base_hand_size = len(self.player1.hand)
-		print ("Starting hand size ", str(base_hand_size))
 		num_victory_cards = len(self.player1.hand.get_cards_by_type("Victory"))
 		
 		tu.send_input(self.player1, "play", "Crossroads")
 		num_actions = self.player1.actions
-		print ("starting vc is ", str(num_victory_cards))
-		print ("The hand len after 1 cr is ", str(len(self.player1.hand)))
-		print ("The hand after 1 cr is ", str(self.player1.hand.card_array()))
 		self.assertTrue(num_actions == 3)
 		self.assertTrue(len(self.player1.hand ) == num_victory_cards + base_hand_size - 1)
 		base_hand_size = len(self.player1.hand)
 		num_victory_cards = len(self.player1.hand.get_cards_by_type("Victory"))
 		deck_size = len(self.player1.deck)
 
-
 		tu.send_input(self.player1, "play", "Crossroads")
 		self.assertTrue(self.player1.actions == num_actions - 1)
-		print ("hand size after 2 cr is ", str(base_hand_size))
-		print ("The hand after 2 cr is ", str(self.player1.hand.card_array()))
-		print ("vc cards after 2 cr is ", str(num_victory_cards))
 		expected_drawn = min(deck_size, num_victory_cards)
 		self.assertTrue(len(self.player1.hand) == expected_drawn + base_hand_size - 1) 
 
