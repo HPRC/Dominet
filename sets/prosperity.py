f1e946ea7af719152123d809eae0620e5062fd0f
Henry Phu <hphu@bu.edu>
2015-10-01 22:24:15 -0400

    fix bug with mint not auto selecting

--- sets/prosperity.py
+++ sets/prosperity.py
@@ -391,7 +391,9 @@ class Mint(crd.Card):
 			self.game.announce("-- but there were no treasures to reveal")
 			crd.Card.on_finished(self, False, False)
 		elif len(treasures) == 1:
-			self.reveal(treasures)
+			self.game.announce("-- revealing " + treasures[0].log_string + ", gaining a copy of it.")
+			yield self.played_by.gain(treasures[0])
+			crd.Card.on_finished(self, False, False)
 		else:
 			selection = yield self.played_by.select(1, 1, treasures, "Choose a card to reveal")
 			self.game.announce("-- revealing " + self.game.log_string_from_title(selection[0]) + ", gaining a copy of it.")
