a53628d231d549a0bd26e4c2d6ffd4c8dce3c2ec
Henry Phu <hphu@bu.edu>
2015-10-24 22:36:04 -0400

    fix plural logs for spies and ironworks

--- sets/intrigue.py
+++ sets/intrigue.py
@@ -466,6 +466,8 @@ class Ironworks(crd.Card):
 
 		yield self.played_by.gain(selection[0], True)
 		post_gain()
+	def log_string(self, plural=False):
+		return "".join(["<span class='label label-default'>", self.title, "</span>"])
 
 
 class Mining_Village(crd.Card):
