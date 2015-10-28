f7f13c157f1a797281dc7eebd94b98b96d492069
Henry Phu <hphu@bu.edu>
2015-10-27 22:24:27 -0400

    changed waiting_on to be a set

--- waitHandler.py
+++ waitHandler.py
@@ -5,8 +5,8 @@ class WaitHandler():
 
 	def __init__(self, player):
 		self.player = player
-		# on = list of player names waiting for, callback called after select/gain gets response, 
-		self.waiting_on = []
+		# on = set of player names waiting for, callback called after select/gain gets response, 
+		self.waiting_on = set()
 		self.msg = ""
 		#locked = set of player names we ignore auto updates (and keep waiting) until manually removed from locked set
 		self.locked = set()
@@ -21,8 +21,7 @@ class WaitHandler():
 		self.player.write_json(command="updateMode", mode="wait", msg="Waiting for " + self.waiting_on_string() + " " + self.msg)
 
 	def append_wait(self, to_append):
-		if not self.is_waiting_on(to_append):
-			self.waiting_on.append(to_append.name)
+		self.waiting_on.add(to_append.name)
 
 	def notify(self, notifier):
 		if not notifier.name in self.locked:
