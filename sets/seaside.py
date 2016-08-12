5c222ec933d28440e7c0cc5a195eaa648dbba208
Refbot <chris.bartolomei@gmail.com>
2016-08-12 09:23:28 -0400

    implemented wharf with unit tests

--- sets/seaside.py
+++ sets/seaside.py
@@ -3,24 +3,47 @@ import tornado.gen as gen
 
 
 class Lighthouse(crd.Duration):
-  def __init__(self, game, played_by):
-    crd.Card.__init__(self, game, played_by)
-    self.title = "Lighthouse"
-    self.description = "{} Now and at the start of your next turn {}. \
-    While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
-    self.price = 2
-    self.type = "Action|Duration"
+	def __init__(self, game, played_by):
+		crd.Card.__init__(self, game, played_by)
+		self.title = "Lighthouse"
+		self.description = "{} Now and at the start of your next turn {}. \
+		While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
+		self.price = 2
+		self.type = "Action|Duration"
 
-  def play(self, skip=False):
-    crd.Duration.play(self, skip)
-    self.played_by.balance += 1
-    self.played_by.actions += 1
-    self.played_by.protection = 9999
-    self.game.announce("-- gaining +$1 and +1 action")
-    crd.Duration.on_finished(self)
+	def play(self, skip=False):
+		crd.Duration.play(self, skip)
+		self.played_by.balance += 1
+		self.played_by.actions += 1
+		self.played_by.protection = 9999
+		self.game.announce("-- gaining +$1 and +1 action")
+		crd.Duration.on_finished(self)
 
-  def duration(self):
-    crd.Duration.duration(self)
-    self.played_by.balance += 1
-    self.game.announce("-- gaining +$1")
-    self.played_by.protection = 0
+	def duration(self):
+		crd.Duration.duration(self)
+		self.played_by.balance += 1
+		self.game.announce("-- gaining +$1")
+		self.played_by.protection = 0
+
+
+class Wharf(crd.Duration):
+	def __init__(self, game, played_by):
+		crd.Card.__init__(self, game, played_by)
+		self.title = "Wharf"
+		self.description = "Now and at the start of your next turn " \
+											 "{} {}.".format(crd.format_draw(2), crd.format_buys(1))
+		self.price = 5
+		self.type = "Action|Duration"
+
+	def play(self, skip=False):
+		crd.Duration.play(self, skip)
+		drawn = self.played_by.draw(2)
+		self.played_by.buys += 1
+		self.game.announce("-- gaining +1 Buy and drawing " + drawn + " cards")
+		crd.Duration.on_finished(self)
+
+	def duration(self):
+		crd.Duration.duration(self)
+		drawn = self.played_by.draw(2)
+		self.played_by.buys += 1
+		self.game.announce(" -- gaining +1 Buy and drawing " + drawn + " cards")
--- tests/seaside_tests.py
+++ tests/seaside_tests.py
@@ -14,27 +14,39 @@ import tests.test_utils as tu
 
 
 class TestSeaside(tornado.testing.AsyncTestCase):
-  def setUp(self):
-    super().setUp()
-    self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
-    self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
-    self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
-    self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
-    self.game.players = [self.player1, self.player2, self.player3]
-    for i in self.game.players:
-      i.game = self.game
-    self.game.start_game()
-    self.player1.take_turn()
+	def setUp(self):
+		super().setUp()
+		self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
+		self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
+		self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
+		self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
+		self.game.players = [self.player1, self.player2, self.player3]
+		for i in self.game.players:
+			i.game = self.game
+		self.game.start_game()
+		self.player1.take_turn()
 
-  def test_Lighthouse(self):
-    tu.print_test_header("test Lighthouse")
-    lighthouse = sea.Lighthouse(self.game, self.player1)
-    lighthouse.play()
-    militia = base.Militia(self.game, self.player2)
-    self.assertTrue(militia.is_blocked(self.player1))
-    self.assertTrue(militia.is_blocked(self.player1))
-    lighthouse.duration()
-    self.assertFalse(militia.is_blocked(self.player1))
+	def test_Lighthouse(self):
+		tu.print_test_header("test Lighthouse")
+		lighthouse = sea.Lighthouse(self.game, self.player1)
+		lighthouse.play()
+		militia = base.Militia(self.game, self.player2)
+		self.assertTrue(militia.is_blocked(self.player1))
+		self.assertTrue(militia.is_blocked(self.player1))
+		lighthouse.duration()
+		self.assertFalse(militia.is_blocked(self.player1))
+
+	def test_Wharf(self):
+		tu.print_test_header("test Wharf")
+		wharf = sea.Wharf(self.game, self.player1)
+		wharf.play()
+		self.assertTrue(len(self.player1.hand) == 7)
+		self.assertTrue(self.player1.buys == 2)
+
+		wharf.duration()
+		self.assertTrue(len(self.player1.hand) == 9)
+		self.assertTrue(self.player1.buys == 3)
+
 
 if __name__ == '__main__':
-    unittest.main()
+		unittest.main()
