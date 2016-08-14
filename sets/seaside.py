8d9389cd8b04af0a5df03044a97b2b1bc8dde289
Refbot <chris.bartolomei@gmail.com>
2016-08-14 07:26:17 -0400

    implemented sea hag with unit tests

--- sets/seaside.py
+++ sets/seaside.py
@@ -3,24 +3,53 @@ import tornado.gen as gen
 
 
 class Lighthouse(crd.Duration):
-  def __init__(self, game, played_by):
-    crd.Card.__init__(self, game, played_by)
-    self.title = "Lighthouse"
-    self.description = "{} Now and at the start of your next turn {}. \
-    While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
-    self.price = 2
-    self.type = "Action|Duration"
+	def __init__(self, game, played_by):
+		crd.Card.__init__(self, game, played_by)
+		self.title = "Lighthouse"
+		self.description = "{} Now and at the start of your next turn {}. \
+		While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
+		self.price = 2
+		self.type = "Action|Duration"
 
-  def play(self, skip=False):
-    crd.Duration.play(self, skip)
-    self.played_by.balance += 1
-    self.played_by.actions += 1
-    self.played_by.protection = 9999
-    self.game.announce("-- gaining +$1 and +1 action")
-    crd.Duration.on_finished(self)
+	def play(self, skip=False):
+		crd.Duration.play(self, skip)
+		self.played_by.balance += 1
+		self.played_by.actions += 1
+		self.played_by.protection = 9999
+		self.game.announce("-- gaining +$1 and +1 action")
+		crd.Duration.on_finished(self)
 
-  def duration(self):
-    crd.Duration.duration(self)
-    self.played_by.balance += 1
-    self.game.announce("-- gaining +$1")
-    self.played_by.protection = 0
+	def duration(self):
+		crd.Duration.duration(self)
+		self.played_by.balance += 1
+		self.game.announce("-- gaining +$1")
+		self.played_by.protection = 0
+
+class Sea_Hag(crd.AttackCard):
+	def __init__(self, game, played_by):
+		crd.AttackCard.__init__(self, game, played_by)
+		self.title = "Sea Hag"
+		self.description = "Each other player discards the top card of their deck, " \
+		                   "then gains a Curse putting it on top of their deck"
+		self.price = 4
+		self.type = "Action|Attack"
+
+	@gen.coroutine
+	def play(self, skip=False):
+		crd.AttackCard.play(self, skip)
+		yield crd.AttackCard.check_reactions(self, self.played_by.get_opponents())
+
+	@gen.coroutine
+	def attack(self):
+		yield self.fire(self.played_by.get_left_opponent())
+
+	@gen.coroutine
+	def fire(self, player):
+		if crd.AttackCard.fire(self, player):
+			topdeck = player.topdeck()
+			player.discard([topdeck.title], player.discard_pile)
+
+			self.game.announce('-- ' + player.name_string() + ' discards ' + self.game.log_string_from_title(topdeck.title))
+			player.gain_to_deck('Curse')
+
+		yield crd.AttackCard.get_next(self, player)
--- tests/seaside_tests.py
+++ tests/seaside_tests.py
@@ -14,27 +14,34 @@ import tests.test_utils as tu
 
 
 class TestSeaside(tornado.testing.AsyncTestCase):
-  def setUp(self):
-    super().setUp()
-    self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
-    self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
-    self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
-    self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
-    self.game.players = [self.player1, self.player2, self.player3]
-    for i in self.game.players:
-      i.game = self.game
-    self.game.start_game()
-    self.player1.take_turn()
+	def setUp(self):
+		super().setUp()
+		self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
+		self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
+		self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
+		self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
+		self.game.players = [self.player1, self.player2, self.player3]
+		for i in self.game.players:
+			i.game = self.game
+		self.game.start_game()
+		self.player1.take_turn()
 
-  def test_Lighthouse(self):
-    tu.print_test_header("test Lighthouse")
-    lighthouse = sea.Lighthouse(self.game, self.player1)
-    lighthouse.play()
-    militia = base.Militia(self.game, self.player2)
-    self.assertTrue(militia.is_blocked(self.player1))
-    self.assertTrue(militia.is_blocked(self.player1))
-    lighthouse.duration()
-    self.assertFalse(militia.is_blocked(self.player1))
+	def test_Lighthouse(self):
+		tu.print_test_header("test Lighthouse")
+		lighthouse = sea.Lighthouse(self.game, self.player1)
+		lighthouse.play()
+		militia = base.Militia(self.game, self.player2)
+		self.assertTrue(militia.is_blocked(self.player1))
+		self.assertTrue(militia.is_blocked(self.player1))
+		lighthouse.duration()
+		self.assertFalse(militia.is_blocked(self.player1))
+
+	def test_Sea_Hag(self):
+		tu.print_test_header("test Sea Hag")
+		sea_hag = sea.Sea_Hag(self.game, self.player1)
+		sea_hag.play()
+		self.assertTrue(len(self.player2.discard_pile) == 1)
+		self.assertTrue(self.player2.topdeck().title == 'Curse')
 
 if __name__ == '__main__':
-    unittest.main()
+		unittest.main()
