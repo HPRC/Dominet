c106301958d1a7d5d736a294e62928f992bcb9ef
Refbot <chris.bartolomei@gmail.com>
2016-08-14 14:18:30 -0400

    implemented explorer with unit tests

--- sets/seaside.py
+++ sets/seaside.py
@@ -3,24 +3,50 @@ import tornado.gen as gen
 
 
 class Lighthouse(crd.Duration):
-  def __init__(self, game, played_by):
-    crd.Card.__init__(self, game, played_by)
-    self.title = "Lighthouse"
-    self.description = "{} Now and at the start of your next turn {}. \
-    While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
-    self.price = 2
-    self.type = "Action|Duration"
+	def __init__(self, game, played_by):
+		crd.Card.__init__(self, game, played_by)
+		self.title = "Lighthouse"
+		self.description = "{} Now and at the start of your next turn {}. \
+		While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
+		self.price = 2
+		self.type = "Action|Duration"
 
-  def play(self, skip=False):
-    crd.Duration.play(self, skip)
-    self.played_by.balance += 1
-    self.played_by.actions += 1
-    self.played_by.protection = 9999
-    self.game.announce("-- gaining +$1 and +1 action")
-    crd.Duration.on_finished(self)
+	def play(self, skip=False):
+		crd.Duration.play(self, skip)
+		self.played_by.balance += 1
+		self.played_by.actions += 1
+		self.played_by.protection = 9999
+		self.game.announce("-- gaining +$1 and +1 action")
+		crd.Duration.on_finished(self)
 
-  def duration(self):
-    crd.Duration.duration(self)
-    self.played_by.balance += 1
-    self.game.announce("-- gaining +$1")
-    self.played_by.protection = 0
+	def duration(self):
+		crd.Duration.duration(self)
+		self.played_by.balance += 1
+		self.game.announce("-- gaining +$1")
+		self.played_by.protection = 0
+
+
+class Explorer(crd.Card):
+	def __init__(self, game, played_by):
+		crd.Card.__init__(self, game, played_by)
+		self.title = "Explorer"
+		self.description = "You may reveal a Province card from your hand. If you do, gain a Gold card, putting it into your hand." \
+		                   "Otherwise, gain a Silver card, putting it into your hand"
+		self.price = 5
+		self.type = "Action"
+
+	@gen.coroutine
+	def play(self, skip=False):
+		crd.Card.play(self, skip)
+
+		selected = False
+		if self.played_by.hand.get_count('Province') > 0:
+			selection = yield self.played_by.select(1, 1, ['Yes', 'No'], "Reveal Province?")
+			if selection[0] == 'Yes':
+				self.played_by.gain_to_hand('Gold')
+				selected = True
+
+		if not selected:
+			self.played_by.gain_to_hand('Silver')
+
+		crd.Card.on_finished(self)
--- tests/seaside_tests.py
+++ tests/seaside_tests.py
@@ -14,27 +14,42 @@ import tests.test_utils as tu
 
 
 class TestSeaside(tornado.testing.AsyncTestCase):
-  def setUp(self):
-    super().setUp()
-    self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
-    self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
-    self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
-    self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
-    self.game.players = [self.player1, self.player2, self.player3]
-    for i in self.game.players:
-      i.game = self.game
-    self.game.start_game()
-    self.player1.take_turn()
+	def setUp(self):
+		super().setUp()
+		self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
+		self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
+		self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
+		self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
+		self.game.players = [self.player1, self.player2, self.player3]
+		for i in self.game.players:
+			i.game = self.game
+		self.game.start_game()
+		self.player1.take_turn()
 
-  def test_Lighthouse(self):
-    tu.print_test_header("test Lighthouse")
-    lighthouse = sea.Lighthouse(self.game, self.player1)
-    lighthouse.play()
-    militia = base.Militia(self.game, self.player2)
-    self.assertTrue(militia.is_blocked(self.player1))
-    self.assertTrue(militia.is_blocked(self.player1))
-    lighthouse.duration()
-    self.assertFalse(militia.is_blocked(self.player1))
+	def test_Lighthouse(self):
+		tu.print_test_header("test Lighthouse")
+		lighthouse = sea.Lighthouse(self.game, self.player1)
+		lighthouse.play()
+		militia = base.Militia(self.game, self.player2)
+		self.assertTrue(militia.is_blocked(self.player1))
+		self.assertTrue(militia.is_blocked(self.player1))
+		lighthouse.duration()
+		self.assertFalse(militia.is_blocked(self.player1))
+
+	@tornado.testing.gen_test
+	def test_Explorer(self):
+		tu.print_test_header("test Explorer")
+		explorer = sea.Explorer(self.game, self.player1)
+		explorer.play()
+		self.assertTrue(self.player1.hand.get_count('Silver') == 1)
+
+		self.player1.actions += 1
+
+		province = supply_cards.Province(self.game, self.player1)
+		self.player1.hand.add(province)
+		explorer.play()
+		yield tu.send_input(self.player1, "post_selection", ["Yes"])
+		self.assertTrue(self.player1.hand.get_count('Gold') == 1)
 
 if __name__ == '__main__':
-    unittest.main()
+		unittest.main()
