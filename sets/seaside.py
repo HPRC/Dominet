cb466338b544868e85d74472913dc96863ede13a
Refbot <chris.bartolomei@gmail.com>
2016-08-13 22:53:08 -0400

    implemented pearl diver with unit tests

--- sets/seaside.py
+++ sets/seaside.py
@@ -3,24 +3,54 @@ import tornado.gen as gen
 
 
 class Lighthouse(crd.Duration):
-  def __init__(self, game, played_by):
-    crd.Card.__init__(self, game, played_by)
-    self.title = "Lighthouse"
-    self.description = "{} Now and at the start of your next turn {}. \
-    While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
-    self.price = 2
-    self.type = "Action|Duration"
+	def __init__(self, game, played_by):
+		crd.Card.__init__(self, game, played_by)
+		self.title = "Lighthouse"
+		self.description = "{} Now and at the start of your next turn {}. \
+		While this is in play, you are unaffected by attack cards".format(crd.format_actions(1), crd.format_money(1))
+		self.price = 2
+		self.type = "Action|Duration"
 
-  def play(self, skip=False):
-    crd.Duration.play(self, skip)
-    self.played_by.balance += 1
-    self.played_by.actions += 1
-    self.played_by.protection = 9999
-    self.game.announce("-- gaining +$1 and +1 action")
-    crd.Duration.on_finished(self)
+	def play(self, skip=False):
+		crd.Duration.play(self, skip)
+		self.played_by.balance += 1
+		self.played_by.actions += 1
+		self.played_by.protection = 9999
+		self.game.announce("-- gaining +$1 and +1 action")
+		crd.Duration.on_finished(self)
 
-  def duration(self):
-    crd.Duration.duration(self)
-    self.played_by.balance += 1
-    self.game.announce("-- gaining +$1")
-    self.played_by.protection = 0
+	def duration(self):
+		crd.Duration.duration(self)
+		self.played_by.balance += 1
+		self.game.announce("-- gaining +$1")
+		self.played_by.protection = 0
+
+
+class Pearl_Diver(crd.Card):
+	def __init__(self, game, played_by):
+		crd.Card.__init__(self, game, played_by)
+		self.title = "Pearl Diver"
+		self.description = "{} {}" \
+											 "While this is in play, you are unaffected by attack cards".format(crd.format_draw(1), crd.format_actions(1))
+		self.price = 2
+		self.type = "Action"
+
+	@gen.coroutine
+	def play(self, skip=False):
+		crd.Card.play(self, skip)
+		self.played_by.actions += 1
+		drawn = self.played_by.draw(1)
+		self.game.announce("Drawing " + drawn + " cards and gaining + 1 actions")
+
+		if len(self.played_by.deck) == 0:
+			self.played_by.shuffle_discard_to_deck()
+
+		bottom_card = self.played_by.deck[0].title
+		selection = yield self.played_by.select(1, 1, ["Yes", "No"], "The bottom card of your deck is " + bottom_card.title() + " "
+								"Move it to the top of your deck?")
+
+		if selection[0] == "Yes":
+			bottom_card = self.played_by.deck.pop(0)
+			self.played_by.deck.append(bottom_card)
+
+		crd.Card.on_finished(self)
--- tests/seaside_tests.py
+++ tests/seaside_tests.py
@@ -14,27 +14,40 @@ import tests.test_utils as tu
 
 
 class TestSeaside(tornado.testing.AsyncTestCase):
-  def setUp(self):
-    super().setUp()
-    self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
-    self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
-    self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
-    self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
-    self.game.players = [self.player1, self.player2, self.player3]
-    for i in self.game.players:
-      i.game = self.game
-    self.game.start_game()
-    self.player1.take_turn()
+	def setUp(self):
+		super().setUp()
+		self.player1 = c.DmClient("player1", 0, tu.PlayerHandler())
+		self.player2 = c.DmClient("player2", 1, tu.PlayerHandler())
+		self.player3 = c.DmClient("player3", 2, tu.PlayerHandler())
+		self.game = g.DmGame([self.player1, self.player2, self.player3], [], [], test=True)
+		self.game.players = [self.player1, self.player2, self.player3]
+		for i in self.game.players:
+			i.game = self.game
+		self.game.start_game()
+		self.player1.take_turn()
 
-  def test_Lighthouse(self):
-    tu.print_test_header("test Lighthouse")
-    lighthouse = sea.Lighthouse(self.game, self.player1)
-    lighthouse.play()
-    militia = base.Militia(self.game, self.player2)
-    self.assertTrue(militia.is_blocked(self.player1))
-    self.assertTrue(militia.is_blocked(self.player1))
-    lighthouse.duration()
-    self.assertFalse(militia.is_blocked(self.player1))
+	def test_Lighthouse(self):
+		tu.print_test_header("test Lighthouse")
+		lighthouse = sea.Lighthouse(self.game, self.player1)
+		lighthouse.play()
+		militia = base.Militia(self.game, self.player2)
+		self.assertTrue(militia.is_blocked(self.player1))
+		self.assertTrue(militia.is_blocked(self.player1))
+		lighthouse.duration()
+		self.assertFalse(militia.is_blocked(self.player1))
+
+	@tornado.testing.gen_test
+	def test_Pearl_Diver(self):
+		tu.print_test_header("test Pearl Diver")
+		pearl_diver = sea.Pearl_Diver(self.game, self.player1)
+		pearl_diver.play()
+		self.assertTrue(len(self.player1.hand) == 6)
+		self.assertTrue(self.player1.actions == 1)
+		bottom_deck = self.player1.deck[0]
+		yield tu.send_input(self.player1, "post_selection", ["Yes"])
+		self.assertTrue(self.player1.topdeck().title == bottom_deck.title)
+
+
 
 if __name__ == '__main__':
-    unittest.main()
+		unittest.main()
